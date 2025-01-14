016d4f0532944d6c6912c5ab60666b5caffc9e66
simizu-masato <m.shimizu@trigger4.com>
2025-01-14 15:52:55 +0900

    ドメインモデルへの変換

--- server/domains/models/pokemons/index.ts
+++ server/domains/models/pokemons/index.ts
@@ -1,4 +1,16 @@
 import z from 'zod';
+import type { Pokemon as PokemonFromPokeApi } from '~/server/infrastructures/pokeapi';
+
+// ポケモン一覧ドメインモデルのスキーマ
+const pokemonsSchema = z.object({
+	id: z.number(),
+	name: z.string(),
+	image: z.string(),
+	types: z.string().array(),
+});
+
+// スキーマから型を取り出す
+export type Pokemons = z.infer<typeof pokemonsSchema>;
 
 // クエリのスキーマ
 const querySchema = z
@@ -14,4 +26,14 @@ export type PokemonsQuery = z.infer<typeof querySchema>;
 // クエリのバリデーション
 export const validatePokemonsQuery = (query: unknown): PokemonsQuery => {
 	return querySchema.parse(query);
+};
+
+// PokeAPIから受け取ったデータをポケモン一覧ドメインモデルへ変換
+export const convert = (pokemon: PokemonFromPokeApi): Pokemons => {
+	return pokemonsSchema.parse({
+		id: pokemon.id,
+		name: pokemon.name,
+		image: pokemon.sprites.front_default,
+		types: pokemon.types.map((type) => type.type.name),
+	});
 };
