a5d0b168ef99e8c2a5f8181209f1ccc93b565c96
simizu-masato <m.shimizu@trigger4.com>
2025-01-15 11:56:13 +0900

    layoutスタイル調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
-	<div>
-		<slot></slot>
+	<div class="flex min-h-screen flex-col bg-gray-100 text-gray-700">
+		<header class="py-4 nm-flat-gray-100">
+			<h1 class="mx-auto max-w-[1200px] px-4 text-2xl font-bold">
+				<NuxtLink to="/"> PokeDex </NuxtLink>
+			</h1>
+		</header>
+		<main class="mx-auto w-full max-w-[1200px] grow">
+			<slot></slot>
+		</main>
+		<footer class="flex items-center justify-center gap-x-1 pb-8">
+			<p class="text-sm opacity-80 md:text-base">© PokeDex created by</p>
+			<NuxtLink
+				to="https://x.com/xxx"
+				target="_blank"
+				class="text-sm opacity-80 md:text-base"
+				>xxx</NuxtLink
+			>
+		</footer>
 	</div>
 </template>
